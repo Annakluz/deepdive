0a82ddaa5b2bd1204183eb14c66a5ffd3e75f714
Annakluz <anna.luz.duran@gmail.com>
2017-10-30 05:15:43 -0600

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'DeepDive';
+  nombre ="";
+
+  constructor(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; 
+import { HttpModule } from '@angular/http'; 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  {path:'', component: AppComponent},
+  {path:'login', component: AppComponent}
+];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD0bHZWvGGK_Votp7OZDNdrxcck5gJoyoI",
+  authDomain: "deepdive-1cc1b.firebaseapp.com",
+  databaseURL: "https://deepdive-1cc1b.firebaseio.com",
+  storageBucket: "deepdive-1cc1b.appspot.com",
+  messagingSenderId:"1037549860489"
+};
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
